f323b92949d6c1377aa8ac885cc81132a77c9aef
Sarfaraz Qadir <sarfarazqadir594@gmail.com.com>
2024-09-16 08:05:41 +0500

    new one

--- script.js
+++ script.js
@@ -1,9 +1,152 @@
-// Assignment 11
+// Assignment 12 to 13
+
+// Question 1
+
+// var user = prompt("Enter Anything");
+
+// if(user = user.typeof(String)){
+    
+//     alert("The input is string and Uppercase")
+// }else{
+//     alert("The input is number")
+// }
+// if(String.toUpperCase()){
+//     alert("The input is string and Uppercase")
+// }else if(String.toLowerCase()){
+//     alert("The input is string and Lowercase")
+// }
 
 
-var a = "58.22";
+// Question 2
 
-var b = Math.round(a);
-document.write(typeof(b))
+// var inp1 = +prompt("number 1");
+// var inp2 = +prompt("number 2");
 
-document.write(b);
+// if(inp1 > inp2 ){
+// alert("the larger value is "+inp1);
+// }else if(inp1 == inp2){
+//     alert("the "+inp1+ " value is equal to "+inp2);
+// }
+
+
+// Question 3
+
+// var user = +prompt("Enter a number", 10);
+
+// if (user < 0) {
+//   alert("The number is negative");
+// } else if (user > 0) {
+//   alert("The number is positive");
+// }else if(user == 0){
+//     alert("The number is zero");
+// }
+
+
+
+
+// Question 4
+
+// var user = prompt("Write a letter");
+
+// if(user == "a"){
+//     alert("It's a Vowel");
+// }else if(user == "e"){
+//     alert("It's a Vowel");
+// }else if(user == "i"){
+//     alert("It's a Vowel");
+// }else if(user == "o"){
+//     alert("It's a Vowel");
+// }else if(user == "u"){
+//     alert("It's a Vowel");
+// }else{
+//     alert("It's a consonant");
+// }
+
+// Question 5
+
+// var user = prompt("Password");
+
+// var password = "sarfaraz";
+
+// if(password == user){
+//     alert("Correct!");
+// }else{
+//     alert("Incorrect password")
+// }
+
+// Question 6
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }else{
+// greeting = "Good evening";
+// }
+
+// Question 7
+
+// var user = +prompt("Enter Hours");
+
+// if(user == 13){
+//     alert("It's 1pm");
+// }else if(user == 14){
+//     alert("It's 2pm");
+// }else if(user == 15){
+//     alert("It's 3pm");
+// }else if(user == 16){
+//     alert("It's 4pm");
+// }else if(user == 17){
+//     alert("It's 5pm");
+// }else if(user == 18){
+//     alert("It's 6pm");
+// }else if(user == 19){
+//     alert("It's 7pm");
+// }else if(user == 20){
+//     alert("It's 8pm");
+// }else if(user == 21){
+//     alert("It's 9pm");
+// }else if(user == 22){
+//     alert("It's 10pm");
+// }else if(user == 23){
+//     alert("It's 11pm");
+// }else if(user == 24){
+//     alert("It's 12am");
+// }else{
+//     alert("It's "+user+"am")
+// }
+
+
+
+// Assignment 14 to 16
+
+// Question No 8
+
+// var StudentsName = ["Sarfaraz","Shams","Irshad"];
+
+// var StudentsMarks = [320,230,480];
+
+// Question No 10
+
+// var StudentsScore = [320,230,480,120];
+
+// var sort = StudentsScore;
+
+// document.write("Score of Students : "+StudentsScore);
+// document.write("<br>Ordered Score of Students : "+StudentsScore.sort());
+
+
+// Question No 11
+
+// var city = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+
+// document.write("Cities List : <br>"+city);
+// document.write("<br>Selected Cities : <br>"+city.slice(0,2));
+
+// Question No 11
+
+// var arr = ["This", "is", "my", "cat"];
+
+// document.write("Array : <br>"+arr);
+// document.write("<br>String : <br>"+arr.join(" "));
+
